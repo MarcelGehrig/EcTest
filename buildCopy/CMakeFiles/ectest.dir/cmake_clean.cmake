12c6cbf0995a102fff0d8acbfad789b73aac588a
MarcelGehrig <marcel.gehrig@ntb.ch>
2017-02-16 11:31:45 +0100

    stack runs as thread

---
+++ buildCopy/CMakeFiles/ectest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ectest.dir/main.cpp.o"
+  "ectest.pdb"
+  "ectest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ectest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
+#include <string>
 #include <iostream>
+#include <thread>
+
+#include <DCDemoMain.hpp>
+//-f eni_1MAXON.xml -i8254x 1 1 -perf
+void task1(std::string msg1, std::string msg2)
+{
+    std::cout << "task1 says: " << msg1 << msg2 << std::endl;
+}
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+// 	std::cout << argv[0] << std::endl;
+// 	std::cout << argv[1] << std::endl;
+// 	std::cout << argv[2] << std::endl;
+// 	std::cout << argv[3] << std::endl;
+// 	std::cout << sayHello("Bob") << std::endl;
+	
+	
+	std::thread t1(mainEtherCAT, argc, argv);
+	
+	
+	std::cout << "hallo" << std::endl;
+	t1.join();
+	
     return 0;
 }
